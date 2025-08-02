417b2753ca556f83ffb202f3796519282a332c12
spandana2004 <110383668+spandana2004@users.noreply.github.com>
2025-08-02 21:16:14 +0530

    Delete week5/2_StudentApp/src directory

--- week5/2_StudentApp/src/Components/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class About extends Component {
-  render() {
-    return <h2>Welcome to the About page of the Student Management Portal</h2>;
-  }
-}
-
-export default About;
--- week5/2_StudentApp/src/Components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Contact extends Component {
-  render() {
-    return <h2>Welcome to the Contact page of the Student Management Portal</h2>;
-  }
-}
-
-export default Contact;
